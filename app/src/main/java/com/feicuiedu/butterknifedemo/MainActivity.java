a33994b2ce15475c61a2a42a737156e8347b9b03
gqq <gqq519@163.com>
2016-11-02 11:18:32 +0800

    初次提交

---
+++ app/src/main/java/com/feicuiedu/butterknifedemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.feicuiedu.butterknifedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
